256f98e5d6d228650a534e41c6928f691e391d38
Bishnu <thapabishnu20@gmail.com>
2026-03-30 17:01:49 +0545

    add facilities and attracations to homepage

--- core/views.py
+++ core/views.py
@@ -19,7 +19,7 @@ def index(request):
     about = About.objects.first()
     facilities = Facility.objects.all()
     sliders = Slider.objects.filter(is_active=True).order_by('serial')
-    # attractions = Attraction.objects.all().order_by('ordering')
+    attractions = Attraction.objects.all().order_by('ordering')
     offers=Offer.objects.filter(is_active=True).order_by('ordering')
     images = Gallery.objects.all().order_by('ordering')
     # room = Room.objects.first()
@@ -32,7 +32,7 @@ def index(request):
         'facilities': facilities,
         'sliders': sliders,
         'offers':offers,
-        # 'attractions': attractions,
+        'attractions': attractions,
         'images': images,
         # 'room': room,
         'otas': otas,
